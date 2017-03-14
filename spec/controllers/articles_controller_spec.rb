77c5972c8b1c52f45b3e7e2cadff211aaf91d621
Lenka Rohackova <lenkamailbox@gmail.com>
2017-03-14 17:34:46 +1100

    tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 RSpec.describe ArticlesController, type: :controller  do
 	describe 'GET #show' do
 		it "assigns the requested article to @article" do
-			article = create(:article)
-			get :show, id: article
+			article = create(:article) # same as rails consol> article =FactoryGirl.create :article
+			get :show, id: article		
 			expect(assigns(:article)).to eq article
+		end
+
+		it "renders the :show template" do
+			article = create(:article)
+			get :show, id: article 
+			binding.pry
+			expect(response).to render_template :show
+			binding.pry
 		end
 	end
 end
